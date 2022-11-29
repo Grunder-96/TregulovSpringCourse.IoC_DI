ab12285a5871036db57c1b31bd86d73a7b182268
Grigoriy Taran <grunder-96@mail.ru>
2022-11-29 15:34:51 +0300

    add lesson 8

--- src/main/java/com/zaurtregulov/spring/IoC_DI/spring_introduction/Person.java
+++ src/main/java/com/zaurtregulov/spring/IoC_DI/spring_introduction/Person.java
@@ -4,8 +4,18 @@ public class Person {
 	
 	private Pet pet;
 	
-	public Person(Pet pet) {
+//	public Person(Pet pet) {
+//		System.out.println("Person been is created");
+//		this.pet = pet;
+//	}
+	
+	public Person() {
 		System.out.println("Person been is created");
+	}
+	
+	//Pet -> setPet
+	public void setPet(Pet pet) {
+		System.out.println("Class person: set Pet");
 		this.pet = pet;
 	}
 	
