e4f52739339f089ef234aa4d5202e128bdd7ef54
Grigoriy Taran <grunder-96@mail.ru>
2022-11-29 15:06:40 +0300

    add lesson 7

--- src/main/java/com/zaurtregulov/spring/IoC_DI/spring_introduction/Cat.java
+++ src/main/java/com/zaurtregulov/spring/IoC_DI/spring_introduction/Cat.java
@@ -2,6 +2,10 @@ package com.zaurtregulov.spring.IoC_DI.spring_introduction;
 
 public class Cat implements Pet{
 	
+	public Cat() {
+		System.out.println("Cat been is created");
+	}
+	
 	@Override
 	public void say() {
 		System.out.println("Meow-Meow");
--- src/main/java/com/zaurtregulov/spring/IoC_DI/spring_introduction/Dog.java
+++ src/main/java/com/zaurtregulov/spring/IoC_DI/spring_introduction/Dog.java
@@ -2,6 +2,10 @@ package com.zaurtregulov.spring.IoC_DI.spring_introduction;
 
 public class Dog implements Pet {
 	
+	public Dog() {
+		System.out.println("Dog been is created");
+	}
+	
 	@Override
 	public void say() {
 		System.out.println("Bow-Wow");
