c0e988944778f3e0798659f9b3186f45c06c4f26
Grigoriy Taran <grunder-96@mail.ru>
2022-11-29 15:49:20 +0300

    add lesson 9

--- src/main/java/com/zaurtregulov/spring/IoC_DI/spring_introduction/Person.java
+++ src/main/java/com/zaurtregulov/spring/IoC_DI/spring_introduction/Person.java
@@ -3,6 +3,8 @@ package com.zaurtregulov.spring.IoC_DI.spring_introduction;
 public class Person {
 	
 	private Pet pet;
+	private String surname;
+	private int age;
 	
 //	public Person(Pet pet) {
 //		System.out.println("Person been is created");
@@ -19,6 +21,24 @@ public class Person {
 		this.pet = pet;
 	}
 	
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setSurname(String surname) {
+		System.out.println("Class person: set surname");
+		this.surname = surname;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		System.out.println("Class person: set age");
+		this.age = age;
+	}
+
 	public void callYourPet() {
 		System.out.println("Hello, my lovely pet!");
 		pet.say();
