b6c167b9fc2bd1c0dca33c05b269c5dde3a98753
Grigoriy Taran <grunder-96@mail.ru>
2022-11-29 13:28:38 +0300

    add lesson 5

---
+++ src/main/java/com/zaurtregulov/spring/IoC_DI/spring_introduction/Test2.java
@@ -0,0 +1,13 @@
+package com.zaurtregulov.spring.IoC_DI.spring_introduction;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test2 {
+	
+	public static void main(String[] args) {
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		Pet pet = context.getBean("myPet", Pet.class);
+		pet.say();
+		context.close();
+	}
+}
